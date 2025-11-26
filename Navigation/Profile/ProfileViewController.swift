0104943a83cd8c06ed176dcf998ce0d7ff9dfcb2
Mishenina Evgenia <eamishenina@gmail.com>
2025-11-26 16:07:52 +0300

    Заданы положения и размеры элементов на экране с Auto Layout

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -4,7 +4,17 @@ class ProfileViewController: UIViewController {
 
     private lazy var profileHeaderView: ProfileHeaderView = {
         let view = ProfileHeaderView()
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view
+    }()
+    
+    private lazy var button: UIButton = {
+       let button = UIButton()
+        button.setTitle("New Button", for: .normal)
+        button.layer.backgroundColor = UIColor.systemBlue.cgColor
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override func viewDidLoad() {
@@ -13,15 +23,39 @@ class ProfileViewController: UIViewController {
         view.backgroundColor = .lightGray
         title = "Профиль"
         
+        setupButton()
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-      
-        setupView()
+        
+        setupProfileHeaderView()
     }
 
-    private func setupView() {
+    private func setupProfileHeaderView() {
         view.addSubview(profileHeaderView)
-        profileHeaderView.frame = view.frame
+        
+        NSLayoutConstraint.activate(
+            [
+            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+            ]
+        )
+    }
+    
+    private func setupButton() {
+        view.addSubview(button)
+        
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate(
+            [
+            button.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,constant: 0.0),
+            button.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0.0),
+            button.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            button.heightAnchor.constraint(equalToConstant: 50.0)
+            ]
+        )
     }
 }
